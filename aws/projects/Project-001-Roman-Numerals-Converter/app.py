11e5b01a2a7a287a6f492af195c6143e05348cb5
= <=>
2021-11-02 08:59:43 +0300

    app.py updated

--- aws/projects/Project-001-Roman-Numerals-Converter/app.py
+++ aws/projects/Project-001-Roman-Numerals-Converter/app.py
@@ -24,7 +24,7 @@ def main_post():
     if not 0 < number < 4000:
         return render_template('index.html', developer_name='gaskin', not_valid=True)
 
-    return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='Sezgin')
+    return render_template('result.html', number_decimal = number , number_roman= convert(number), developer_name='gaskin')
 
 if __name__ == '__main__':
     #app.run(debug=True)
